32a156bda1b1539c8324d69510cde2b015652830
tyss <tyss@104.239.213.7>
2017-11-20 17:53:06 +0530

    commiting to git hub 20

--- src/test/java/ObjectRepository/LoginPage.java
+++ src/test/java/ObjectRepository/LoginPage.java
@@ -10,13 +10,15 @@ public class LoginPage
  
  @FindBy(xpath="//input[@id='pass']")
  private WebElement password;
- 
+   
+@FindBy(xpath="//span[.='Login']")
+public WebElement login;
  
  public void loginpage()
  {
 	 username.sendKeys("risingstar61190@gmail.com");
 	 password.sendKeys("takaslk");
-	 
+	 login.click();
 	 
  }
 }
